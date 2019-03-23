b5efe4dc24e87503cf42ac008bd116cda223c1f9
Manjula Jayawardana <manjulajayawardana@gmail.com>
2019-03-23 21:09:21 +0530

    linear layout

---
+++ ToDoList/app/src/main/java/com/slmanju/todolist/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.slmanju.todolist
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { showNewTaskUI() }
+    }
+
+    fun showNewTaskUI() {
+    }
+
+}
