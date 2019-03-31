c66b6f577d1aa0118d9abedb7c3b5cb621982a5f
Manjula Jayawardana <manjulajayawardana@gmail.com>
2019-03-31 23:27:54 +0530

    kotlin hello world

---
+++ blog/src/main/java/com/slmanju/blog/Hello.java
@@ -0,0 +1,9 @@
+package com.slmanju.blog;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
---
+++ blog/src/main/kotlin/com/slmanju/blog/Hello.kt
@@ -0,0 +1,5 @@
+package com.slmanju.blog
+
+fun main(args: Array<String>) {
+    println("Hello World")
+}
